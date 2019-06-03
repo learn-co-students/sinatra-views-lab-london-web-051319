f49780a1380f6dc7b051d823fc278abe50016ee2
github username <github email address>
2019-06-03 14:12:01 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,21 @@
+require "pry"
 class App < Sinatra::Base
 
-	get '/' do
+   get '/' do
 		erb :index
-	end
+   end
+   
+   get '/hello' do
+      erb :hello
+   end
 
+   get '/goodbye' do
+      erb :goodbye
+   end
+
+   get '/date' do
+      erb :date
+      # binding.pry
+   end
 
 end
